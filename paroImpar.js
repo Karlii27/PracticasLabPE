c5a82c63ed513ab48b63e90b0eec6089e7bbb165
Karla <keimi272005@gmail.com>
2024-08-16 11:54:22 -0600

    tarea1

---
+++ paroImpar.js
@@ -0,0 +1,11 @@
+
+function paroImpar(numero){
+    return numero
+}
+
+let numeroparoImpar=paroImpar(4)
+if (numeroparoImpar % 2==0) {
+    console.log("verdadero")
+} else {
+    console.log("falso")
+}
